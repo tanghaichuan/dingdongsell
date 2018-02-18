891ec936d425c6f318e4d1aa6ec2668dbe627cb0
tanghaichuan <764505165@qq.com>
2018-02-18 21:00:21 +0800

    搭建个人页面骨架

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,51 @@
 <template>
   <header>
-    
+    <van-nav-bar
+      title="我的"
+      left-arrow
+      @click-left="onClickLeft"
+    />
   </header>
 </template>
 <script>
 export default {
-  name: 'headerBar'
+  name: 'headerBar',
+  methods: {
+    onClickLeft() {
+      this.$router.go(-1)
+    }
+  }
 }
 </script>
-<style scoped lang="less">
-
+<style lang="less">
+@import '../assets/styles/mixins.less';
+header {
+  .van-nav-bar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: transparent;
+    height: 1.173333rem;
+  }
+  .van-nav-bar__title {
+    height: 100%;
+    display: inline-block;
+    .fs42;
+    line-height: 2;
+    text-align: center;
+  }
+  .van-nav-bar__left {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    .fs(32);
+  }
+  .van-nav-bar__right {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    .fs(32);
+  }
+}
 </style>
